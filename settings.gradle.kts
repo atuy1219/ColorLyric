133c26156b688b540d46b30086735da27f4e1d1a
Atuy <130917473+atuy1219@users.noreply.github.com>
2026-09-06 16:52:02 +0900

    旧Xposedリポジトリを削除

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,6 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
-        maven { url = uri("https://api.xposed.info/") }
     }
 }
 
@@ -12,7 +11,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://api.xposed.info/") }
     }
 }
 
